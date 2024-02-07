b3b23629ea8b65ade2a497880e170f58e8d04a78
Anilkumar1 <156059741+Anil-1kumar@users.noreply.github.com>
2024-02-08 01:15:47 +0530

    Add files via upload

---
+++ Test Suites/Logout_test listener_TS/TC_Logout_test listener_TS.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Logout_test listener_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a6b45969-336b-4c75-8e88-abf63072d350</testSuiteGuid>
+   <testCaseLink>
+      <guid>2505d096-00ae-4322-a2dd-5c0608a783c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logout_test listener/TC_Logout_test listener</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
